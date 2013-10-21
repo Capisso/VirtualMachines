a066db1e248c743ea57d4ebbf38ea07ecd03b5b1
Luke Strickland <luke@axxim.net>
2013-10-21 13:59:34 -0500

    Formatting changes

--- src/Capisso/VirtualMachines/VirtualMachinesServiceProvider.php
+++ src/Capisso/VirtualMachines/VirtualMachinesServiceProvider.php
@@ -2,45 +2,46 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class VirtualMachinesServiceProvider extends ServiceProvider {
+class VirtualMachinesServiceProvider extends ServiceProvider
+{
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('capisso/virtual-machines');
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('capisso/virtual-machines');
 
-        include __DIR__.'/../../routes.php';
-	}
+        include __DIR__ . '/../../routes.php';
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		//
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return array();
+    }
 
 }
